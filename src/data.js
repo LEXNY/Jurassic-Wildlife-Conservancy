1edbdaf0953e9909b23ce4c02cb83f0caa6b72d1
Lex Lightning <lex@Absurd-Reckonings.local>
2024-04-03 17:42:12 -0700

    commit

--- src/data.js
+++ src/data.js
@@ -1,23 +1,49 @@
+// traits
+[
+  // set
+  'entrance',
+  'administration',
+  'enclosure',
+
+  // entity human
+  'human',
+  'mechanic',
+  'administrator',
+  'hunter',
+  'rifle',
+
+  // entity dinosaur
+  'dinosaur',
+].map(e => eval(`const ${e} = 'e'`))
+
+
 export const sets = [
   {
     name: 'Entrance',
-    traits: ['entrance'],
+    traits: {entrance},
     entities: [
-      {name: 'Jo Schwartz', traits: ['human', 'mechanic']},
-      {name: 'Lita Maroon', traits: ['human', 'administrator']},
-      {name: 'Jaque Salls', traits: ['human', 'hunter', 'rifle']},
+      {name: 'Jo Schwartz', traits: {human, mechanic}},
+      {name: 'Lita Maroon', traits: {human, administrator}},
+      {name: 'Jaque Salls', traits: {human, hunter, rifle}},
     ]
   },
   {
     name: 'administration',
-    traits: ['TODO'],
+    traits: {},
     entities: []
   },
   {
     name: 'velociraptor enclosure',
-    traits: ['enclosure', 'electric fence'],
+    traits: {enclosure},
     entities: [
-      {name: 'A-A-A', traits: ['dinosaur']},
+      {name: 'A-A-A', traits: {dinosaur}},
+    ]
+  },
+  {
+    name: 'riding a T-rex',
+    traits: {grappling},
+    entities: [
+      {name: 'HRAH', traits: {dinosaur}},
     ]
   },
 ]
